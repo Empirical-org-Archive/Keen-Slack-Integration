175c692db841d2fe5f52a3de08b5908985ed6abb
RyanNovas <ryan@quill.org>
2015-11-12 15:51:25 -0500

    runs locally

--- example_job.rb
+++ example_job.rb
@@ -1,12 +1,10 @@
-require 'sinatra'
 require 'pushpop'
 require 'pushpop-slack'
-require 'pry'
 
-get "/trigger" do
-  job do
-    slack do |details|
-        message "do i work as a webhook?"
-    end
+
+job do
+  slack do
+    message "fuck webhooks"
+    channel '#general'
   end
 end

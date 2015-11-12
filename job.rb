f5a9100e7ad59f4c82791231e89c291fa007830e
RyanNovas <ryan@quill.org>
2015-11-12 17:24:10 -0500

    all stats reported

--- job.rb
+++ job.rb
@@ -1,17 +1,32 @@
 require 'pushpop'
 require 'pushpop-slack'
 require 'pushpop-keen'
+require 'pry'
+
+
 
 job 'send keen stats to Quill Slack' do
-  every 10.seconds
-    keen do
-      event_collection  'accounts'
-      analysis_type     'count'
-      timeframe         'last_month'
-    end
 
-    slack do |response|
-      message "#{response} students signed in in the last month"
-      channel '#general'
+  every 24.hours
+
+  stats = ["Student created an account", "Teacher created an account", "Student signed in", "Teacher signed in", "Student completed an activity"]
+  stats.each do |stat|
+    keen stat do
+      event_collection  stat
+      analysis_type     'count'
+      timeframe         'last_24_hours'
     end
+  end
+
+  slack do |response, step_responses|
+    binding.pry
+    message "In the last 24 hours: \n
+
+             #{step_responses["Student created an account"]} students created accounts
+             #{step_responses["Teacher created an account"]} teachers created accounts
+             #{step_responses["Teacher signed in"]} teachers signed in
+             #{step_responses["Student signed in"]} students signed in
+             #{step_responses["Student completed an activity"]} students completed activities"
+    channel '#general'
+  end
 end
